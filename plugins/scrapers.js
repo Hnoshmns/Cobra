0e77f12121705c0eae1d0c4722a3a6c4723c8294
SISULA WELGAMAGE <88249205+Sf0gmailcomceb5ace3@users.noreply.github.com>
2021-08-01 19:52:40 +0530

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -475,7 +475,7 @@ if (config.WORKTYPE == 'private') {
                 writer.addTag();
 
                 reply = await message.client.sendMessage(message.jid,config.SONG_UP,MessageType.text);
-                await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.document, {mimetype: Mimetype.mp4audio, ptt: false});
+                await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.document, {filename: title + '.mp3', mimetype: 'audio/mpeg', ptt: false});
             });
     }));
 
